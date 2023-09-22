95e88e537db721f60d422511d8e6b53db382fde3
JIHYE <jye7921@gmail.com>
2023-09-22 17:22:30 +0900

    [unrated] Title: 공배수, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -1,7 +1,9 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int answer = (number % n == 0) && (number%m ==0) ? 1: 0;
-
-        return answer;
+        if(number % n == 0  && number % m== 0) {
+            return 1;
+        } else {
+            return 0; 
+        }
     }
 }
