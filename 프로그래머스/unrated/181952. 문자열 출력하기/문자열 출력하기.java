8bb0526f677ac3543ce7dda74032fcb81befa701
JIHYE <jye7921@gmail.com>
2023-10-09 23:58:07 +0900

    [level unrated] Title: 문자열 출력하기, Time: 143.51 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        System.out.print(a);
+    }
+}
