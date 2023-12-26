b4f4f30b9c46dfd6eedc9bae353f462a37bbe0f9
JIHYE <jye7921@gmail.com>
2023-12-26 23:49:59 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 228 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            String text = scan.nextLine();
+            System.out.println(text);
+
+        }
+    }
+}
