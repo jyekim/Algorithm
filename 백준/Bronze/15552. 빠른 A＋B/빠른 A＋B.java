ad45e9b6c3bc8936d4d9692b28c9e9d3b9a762bd
JIHYE <jye7921@gmail.com>
2024-01-17 23:55:32 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 900 ms, Memory: 237492 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine()); //문제는 스트링값으로 리턴이 되기에 Integer.parseInt로 해줘야함
+        for (int i=0; i<T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a+b+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
