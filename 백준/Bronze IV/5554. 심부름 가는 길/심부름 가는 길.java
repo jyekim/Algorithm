a4470335c13bc2224301c632186d542ff8f4bd18
JIHYE <jye7921@gmail.com>
2024-02-14 23:24:30 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 208 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze IV/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int time =0;
+        for(int i=0; i<4; i++){
+            time += sc.nextInt();
+        }
+        System.out.println(time/60);
+        System.out.println(time%60);
+    }
+}
