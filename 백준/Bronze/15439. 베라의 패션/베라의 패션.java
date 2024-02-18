6df9b6cdc651454345cbfa8f5492cc34f76827d6
JIHYE <jye7921@gmail.com>
2024-02-18 23:36:52 +0900

    [Bronze IV] Title: 베라의 패션, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+//베라의 패션
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        System.out.println(N*(N-1));
+    }
+}
