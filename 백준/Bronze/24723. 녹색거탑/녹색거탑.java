a9dbb0d491eb9b663cc148fe921cbd448967648a
JIHYE <jye7921@gmail.com>
2024-02-28 23:14:05 +0900

    [Bronze IV] Title: 녹색거탑, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int A[] = { 1,2,4, 8, 16, 32};
+        System.out.println(A[N]);
+    }
+}
