224f70932c66c3c549b799a50b36f23fec8195c0
JIHYE <jye7921@gmail.com>
2024-03-07 08:01:42 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        while (true) {
+            String name = scan.next();
+            int age = scan.nextInt();
+            int weight = scan.nextInt();
+
+            if (name.equals("#") && age==0 && weight==0) {
+                break;
+            }
+            if (age > 17 || weight >= 80) {
+                System.out.println(name + " Senior");
+            } else {
+                System.out.println(name + " Junior");
+            }
+        }
+    }
+}
