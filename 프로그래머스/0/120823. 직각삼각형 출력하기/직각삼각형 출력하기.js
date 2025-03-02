9889a4a4cbfc9537babb654059a2278ced9acdd2
JIHYE <jye7921@gmail.com>
2025-03-02 23:55:35 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 46.29 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+rl.on('line', function (line) {
+    let n = Number(line); 
+    for (let i = 1; i <= n; i++) {
+        console.log('*'.repeat(i));
+    }
+    rl.close();
+});
+
+
+
