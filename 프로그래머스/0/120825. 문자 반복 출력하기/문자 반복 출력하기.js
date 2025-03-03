be64243663a738eddb20fafb7d74f104de82e3c7
JIHYE <jye7921@gmail.com>
2025-03-03 22:52:25 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,15 @@
+function solution(my_string, n) {
+    var answer = '';
+    var cnt = 0; 
+    while (cnt <my_string.length ){
+        const repeatCharacter = my_string[cnt]
+        
+    let repeatCnt = 0;
+        while(repeatCnt < n) {
+            answer = answer + repeatCharacter;
+            repeatCnt = repeatCnt + 1;
+        }
+        cnt = cnt + 1;
+    }
+    return answer;
+}
