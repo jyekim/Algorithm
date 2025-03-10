1cee62b8d42aa480ef4501ea6dadb274a1b1b3d4
JIHYE <jye7921@gmail.com>
2025-03-10 23:41:40 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,11 @@
+function solution(age) {
+    var answer = '';
+    const stringAge = String(age);
+    let cnt = 0;
+
+    while(cnt < stringAge.length) {
+        answer = answer + String.fromCharCode(stringAge[cnt].charCodeAt(0) + 49);
+        cnt = cnt + 1;
+    }
+    return answer;
+}
